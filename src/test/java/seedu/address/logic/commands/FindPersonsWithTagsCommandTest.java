24ce65013fb49f0f7d3efcb8e30a5b5ea8c44f5d
tweijie <tweijie216@gmail.com>
2017-10-13 00:35:32 +0800

    add equals to test

--- src/test/java/seedu/address/logic/commands/FindPersonsWithTagsCommandTest.java
+++ src/test/java/seedu/address/logic/commands/FindPersonsWithTagsCommandTest.java
@@ -1,14 +1,47 @@
 package seedu.address.logic.commands;
 
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
+
+import java.util.Collections;
+
+import org.junit.Test;
 
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
+import seedu.address.model.person.PersonContainsTagsPredicate;
 
 public class FindPersonsWithTagsCommandTest {
     private Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
 
+    @Test
+    public void equals() {
+        PersonContainsTagsPredicate firstPredicate =
+                new PersonContainsTagsPredicate(Collections.singletonList("first"));
+        PersonContainsTagsPredicate secondPredicate =
+                new PersonContainsTagsPredicate(Collections.singletonList("second"));
+
+        FindPersonsWithTagsCommand findFirstCommand = new FindPersonsWithTagsCommand(firstPredicate);
+        FindPersonsWithTagsCommand findSecondCommand = new FindPersonsWithTagsCommand(secondPredicate);
+
+        // same object -> returns true
+        assertTrue(findFirstCommand.equals(findFirstCommand));
+
+        // same values -> returns true
+        FindPersonsWithTagsCommand findFirstCommandCopy = new FindPersonsWithTagsCommand(firstPredicate);
+        assertTrue(findFirstCommand.equals(findFirstCommandCopy));
+
+        // different types -> returns false
+        assertFalse(findFirstCommand.equals(1));
+
+        // null -> returns false
+        assertFalse(findFirstCommand.equals(null));
+
+        // different person -> returns false
+        assertFalse(findFirstCommand.equals(findSecondCommand));
+    }
 
 
 }
